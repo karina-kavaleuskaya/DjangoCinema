2cea41fea43208f52ed51a65e5ed3d9294533b3e
karina-kavaleuskaya <155006843+karina-kavaleuskaya@users.noreply.github.com>
2024-03-18 19:25:49 +0300

    problems fake_data.py

--- cinema/fake_data.py
+++ cinema/fake_data.py
@@ -18,3 +18,33 @@ for _ in range(5):
 
 genres = Genre.objects.all()
 
+producers = []
+for _ in range(5):
+    producer_data = {
+        'first_name': fake.first_name(),
+        'last_name': fake.last_name(),
+        'date_of_birth': fake.date_of_birth(minimum_age=18, maximum_age=40),
+        'image': fake.image_url()
+    }
+
+    if fake.random_int(min=0, max=7):
+        producer_data['date_of_death'] = fake.date_of_birth(minimum_age=1, maximum_age=40)
+
+    producers.append(producer_data)
+
+    Producer.objects.create(**producer_data)
+
+
+for _ in range(5):
+    film_data = {
+        'title': fake.sentence(nb_words=3, variable_nb_words=True),
+        'description': fake.paragraph(),
+        'age': fake.random_int(min=0, max=18),
+        'image': fake.image_url(),
+        'producer': fake.random_element(elements=producers),
+        'actors': fake.random_elements(elements=actors_data),
+        'genre': fake.random_elements(elements=genres)
+    }
+
+
+    film = Film.objects.create(**film_data)
